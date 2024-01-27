ed5f47bb754cf58a32245b878aab10d55229e1da
2abet <yemiarabambi@gmail.com>
2024-01-27 15:55:33 +0000

    Version and Documentation Updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='train_time',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     install_requires=[
         'keras',
