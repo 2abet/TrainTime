d147938faee3525e2e6c5f8bda817c88d317f33a
Akinyemi Arabambi <32962207+2abet@users.noreply.github.com>
2025-05-31 11:28:03 +0100

    Docstrings added

--- train_time/traintime.py
+++ train_time/traintime.py
@@ -2,15 +2,54 @@ import time
 from tensorflow.keras.callbacks import Callback
 
 class train_time(Callback):
+    """
+    Keras Callback that tracks and estimates training time per epoch.
+
+    Parameters:
+        time_format (str): Unit of time to display for estimated remaining time.
+                           Options are 'seconds', 'minutes', 'hours', or 'days'.
+                           Default is 'seconds'.
+    """
+    
     def __init__(self, time_format='seconds'):
+        """
+        Initialize the callback with the preferred time format.
+
+        Args:
+            time_format (str): Format to report the estimated remaining time.
+        """
         super().__init__()
         self.time_format = time_format
 
     def on_train_begin(self, logs=None):
+        """
+        Called at the beginning of training. Initializes time tracking variables.
+
+        Args:
+            logs (dict): Currently unused. Included for compatibility.
+        """
         self.times = []
         self.total_time = 0.0
 
+    def on_epoch_begin(self, epoch, logs=None):
+        """
+        Called at the beginning of each epoch. Starts the epoch timer.
+
+        Args:
+            epoch (int): Index of the epoch.
+            logs (dict): Currently unused. Included for compatibility.
+        """
+        self.epoch_start_time = time.time()
+
     def on_epoch_end(self, epoch, logs=None):
+        """
+        Called at the end of each epoch. Records the time taken for the epoch
+        and prints the estimated remaining time in the chosen format.
+
+        Args:
+            epoch (int): Index of the completed epoch.
+            logs (dict): Dictionary of metrics from the training epoch.
+        """
         epoch_time = time.time() - self.epoch_start_time
         self.times.append(epoch_time)
         self.total_time += epoch_time
@@ -31,6 +70,3 @@ class train_time(Callback):
             time_unit = "seconds"
 
         print(f"\nEpoch: {epoch + 1}/{self.params['epochs']}. Estimated time remaining: {remaining_time:.2f} {time_unit}.")
-
-    def on_epoch_begin(self, epoch, logs=None):
-        self.epoch_start_time = time.time()
